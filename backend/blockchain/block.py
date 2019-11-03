0ecb16c619d7748d7c36fa1dd219e3dfdd12bf7c
Oleh <olbalenko@gmail.com>
2019-11-03 15:35:32 +0200

    implemented proof of work

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -6,7 +6,9 @@ GENESIS_DATA = {
     'timestamp': 1,
     'last_hash': 'genesis_last_hash',
     'hash': 'genesis_hash',
-    'data': []
+    'data': [],
+    'difficulty': 3,
+    'nonce': 'genesis_nonce'
 }
 
 
@@ -15,11 +17,13 @@ class Block:
     Block: a unit of storage
     Storage transactions in a blockchain that supports a cryptocurrency
     '''
-    def __init__(self, timestamp, last_hash, hash, data):
+    def __init__(self, timestamp, last_hash, hash, data, difficulty, nonce):
         self.timestamp = timestamp
         self.last_hash = last_hash
         self.hash = hash
         self.data = data
+        self.difficulty = difficulty
+        self.nonce = nonce
 
     def __repr__(self):
         return (
@@ -27,21 +31,32 @@ class Block:
             f'timestamp: {self.timestamp}, '
             f'lash_hash: {self.last_hash}, '
             f'hash: {self.hash}, '
-            f'data: {self.data})'
+            f'data: {self.data}, '
+            f'difficulty: {self.difficulty}, '
+            f'nonce: {self.nonce})'
         )
 
     @staticmethod
     def mine_block(last_block, data):
         '''
-        Mine a block based on the given last_block and data
+        Mine a block based on the given last_block and data, until a block hash
+        is found that meets the leading 0's proof of work requirement
         :param last_block:
         :param data:
         :return:
         '''
         timestamp = int(round(time.time(), 0) + random.randrange(1000, 5000))
         last_hash = last_block.hash
-        hash = crypto_hash(timestamp, last_hash, data)
-        return Block(timestamp, last_hash, hash, data)
+        difficulty = last_block.difficulty
+        nonce = 0
+        hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
+
+        while hash[0:difficulty] != '0' * difficulty:
+            nonce += 1
+            timestamp = int(round(time.time(), 0) + random.randrange(1000, 5000))
+            hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
+
+        return Block(timestamp, last_hash, hash, data, difficulty, nonce)
 
     @staticmethod
     def genesis():
