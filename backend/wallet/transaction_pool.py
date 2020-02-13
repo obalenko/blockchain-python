2717b83def37381b75e1b6e2677996e01d51b365
Oleh <olbalenko@gmail.com>
2020-02-13 14:57:57 +0200

    transaction pool added

---
+++ backend/wallet/transaction_pool.py
@@ -0,0 +1,14 @@
+class TransactionPool:
+    """
+    Transaction pool
+    """
+    def __init__(self):
+        self.transaction_map = {}
+
+    def set_transaction(self, transaction):
+        """
+        Set a transaction in the transaction pool.
+        :param transaction:
+        :return:
+        """
+        self.transaction_map[transaction.id] = transaction
